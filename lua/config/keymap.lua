f5fc9a8d2c71b64a2fdf775204f3be2d3db3febb
Aaron <aaron.stacey@proton.me>
2025-07-17 07:25:28 -0500

    Init

---
+++ lua/config/keymap.lua
@@ -0,0 +1,15 @@
+-- leader keys
+-- vim.g.mapleader = " "
+-- vim.g.maplocalleader = " "
+
+-- lefty command
+vim.keymap.set("n", "~", ":", { desc = "Lefty Command" })
+
+-- telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+
---
+++ lua/config/settings.lua
@@ -0,0 +1,8 @@
+-- ui
+vim.opt.number=true
+vim.opt.relativenumber=true
+
+-- system keyboard
+vim.opt.clipboard = "unnamedplus"
+vim.opt.mouse = "a"
+

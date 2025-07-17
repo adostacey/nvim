cac0639f16ad327a490327418be610efbb2a5391
Aaron <140290205+adostacey@users.noreply.github.com>
2025-07-17 11:38:03 -0500

    update: default colorscheme load

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -7,7 +7,7 @@ vim.opt.smarttab = true
 vim.opt.smartindent = true
 vim.opt.autoindent = true -- Keep identation from previous line
 
--- timeout adjust
+
 vim.opt.timeoutlen = 2000
 
 -- ui
@@ -31,8 +31,14 @@ vim.o.inccommand = 'split'
 vim.o.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 10
+vim.o.scrolloff = 5
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.o.ignorecase = true
 vim.o.smartcase = true
+
+-- gutter for error/warnings
+vim.opt.signcolumn = "yes"
+
+vim.cmd("colorscheme gruvbox")
+

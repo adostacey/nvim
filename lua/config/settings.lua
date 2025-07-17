4da9440f6ac24c0e9f7e1ba748175eb8d8170940
Aaron <140290205+adostacey@users.noreply.github.com>
2025-07-17 11:06:06 -0500

    update: various setting from kickstart

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,3 +1,12 @@
+-- tabs
+vim.opt.expandtab = true -- Convert tabs to spaces
+vim.opt.shiftwidth = 4 -- Amount to indent with << and >>
+vim.opt.tabstop = 4 -- How many spaces are shown per Tab
+vim.opt.softtabstop = 4 -- How many spaces are applied when pressing Tab
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true -- Keep identation from previous line
+
 -- timeout adjust
 vim.opt.timeoutlen = 2000
 
@@ -9,3 +18,21 @@ vim.opt.relativenumber=true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.mouse = "a"
 
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.o.confirm = true
+
+
+-- Preview substitutions live, as you type!
+vim.o.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.o.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 10
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
